b0800af3533b5bb314735b5f9af180497086fbf0
Satish Narayanappa <s.narayanappa@prowareness.com>
2017-08-02 14:40:12 +0530

    recaptcha code

---
+++ RecaptchaSample/RecaptchaSample/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RecaptchaSample.Model;
+using PaulMiami.AspNetCore.Mvc.Recaptcha;
+
+namespace RecaptchaSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [ValidateRecaptcha]
+        [HttpPost]
+        public ActionResult Contact(Contact form)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(ThankYou), new { name = form.Name });
+            }
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult ThankYou(string name)
+        {
+            return View(nameof(ThankYou), name);
+        }
+    }
+}
---
+++ RecaptchaSample/RecaptchaSample/Model/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecaptchaSample.Model
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
